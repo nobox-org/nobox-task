436f17f16ff68a8b606f88c137f07275f4465f72
Precious Olusola <preciousolusola16@gmail.com>
2023-12-22 02:43:40 +0100

    Fixed post list bug

--- src/components/post/PostItem.tsx
+++ src/components/post/PostItem.tsx
@@ -7,17 +7,17 @@ import Reactions from "./Reactions";
 
 
 const PostItem = ({post}: {post: IPost}) => {
-    
+
     return (
         <article>
             <h2>{post.title}</h2>
 
-            <p className='excerpt'>{post.body.substring(0, 75)}</p>
+            <p className='excerpt'>{post.content.substring(0, 75)}</p>
 
             <p className="postCredit">
                 <Link href={`post/${post.id}`}>View Post</Link>
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
+                <PostAuthor userId={post.userId as string} />
+                <TimeAgo timestamp={post.date as string} />
                 <br />
                 <Reactions post={post} />
             </p>
